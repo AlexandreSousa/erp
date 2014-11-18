af3f1e39bf920b4963ad0c95e25dde9ce59ebcc3
alexandre <sousa.akira@gmail.com>
2014-11-18 00:04:01 -0200

    inicio de tudo

--- include/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-function __autoload($classe){
-    if(file_exists("class/".$classe.".Class.php")):
-        include_once("class/".$classe.".Class.php");
-
-    endif;
-}
--- modulos/clientes/add_cliente.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: alexandre
- * Date: 11/11/14
- * Time: 22:10
- */ 
